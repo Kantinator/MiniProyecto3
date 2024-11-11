a67bcf8f46e61c1a56c9c30b3a7232aaa65cf74b
Jimmy Aizprua <jimmy.aizprua@gmail.com>
2024-11-11 15:41:12 -0500

    crear la pantalla de login En la base de datos, colocar solo el rol de administrador y inserte usuario y contrasena del rol de administrador

--- MiniProyecto3/FormAdmin.Designer.cs
+++ MiniProyecto3/FormAdmin.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace MiniProyecto3
 {
-    partial class Form1
+    partial class FormAdmin
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,23 +20,24 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // FormAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(965, 532);
+            this.Name = "FormAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormAdmin";
             this.ResumeLayout(false);
 
         }
@@ -44,4 +45,3 @@
         #endregion
     }
 }
-
---
+++ MiniProyecto3/SQLGYM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniProyecto3
+{
+    internal class SQLGYM
+    {
+        public string cadenaConexion = "Data Source=DESKTOP-2PNP6F6\\SQLBRIEF; Initial Catalog=GimnasioDB; Integrated Security=true;";
+
+        public string VerificarCredenciales(string nombreUsuario, string contrasena)
+        {
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                string query = "SELECT rol FROM Usuario WHERE nombre_usuario = @nombreUsuario AND contrasena = @contrasena";
+                SqlCommand comando = new SqlCommand(query, conexion);
+                comando.Parameters.AddWithValue("@nombreUsuario", nombreUsuario);
+                comando.Parameters.AddWithValue("@contrasena", contrasena);
+
+                try
+                {
+                    conexion.Open();
+                    object resultado = comando.ExecuteScalar();
+
+                    if (resultado != null)
+                    {
+                        return resultado.ToString(); // Devuelve el rol del usuario si las credenciales son correctas
+                    }
+                    else
+                    {
+                        return null; // Retorna null si las credenciales no son correctas
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al verificar credenciales: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
